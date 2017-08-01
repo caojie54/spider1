86fc65ca02ea81adfe2a905d648f4e196d3425bb
caojie <caojiestc@gmail.com>
2017-08-01 16:26:28 +0800

    commit;

--- crawler.py
+++ crawler.py
@@ -3,27 +3,29 @@
 ''' i want get many pictures on taobao website	'''
 import requests
 import re
-main_url = 'http://www.taobao.com'
+main_url = 'https://www.hao123.com/'
 scraped_url = [];
 current_page_url= []
 def scrape(url):
-    print(existed(url))
     if existed(url) == True:
-        print("1")
-        pass
+        print("this page is over")
     else:
-        print("2")
-        r = requests.get(url)
-        get_need_data(r.text)
-        #print(r.text)
-        #add url into scraped_url,
-        scraped_url.append(url)
-        for url in re.findall("<a href=.*?>",r.text,re.S):
-            print(url)
+        try:    
+            r = requests.get(url)
+            get_need_data(r.text)
+            #print(r.text)
+            #add url into scraped_url,
+            scraped_url.append(url)
+            with open("/home/caojie/url.txt",'a') as f:
+                f.write(url)
+            for url in re.findall("<a href=(.*?)>",r.text,re.S):
+                print(url)
+                scrape(url)
+        except Exception as e:
+            print("Exception occur %s" % e)
 
 #existed in scraped_url,already been scraped
 def existed(url):
-    print(scraped_url)
     if url in scraped_url:
 	    return True
     return False
